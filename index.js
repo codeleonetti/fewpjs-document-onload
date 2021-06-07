9ce5da4372524ee1ded89dacd82938f5d7c192d1
john <john.d.leonetti@gmail.com>
2021-06-07 11:02:29 -0700

    commit tree de8acfda80bfd703d2a20edba8f25e92e5f9b7e7

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    
+
+
+    document.getElementById("text").innerHTML = "This is really cool!";
+    
+})
+
+
+   
